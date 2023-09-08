e05f3afa8eee2925a874c67edb117784999d16ab
ajmyers89 <144396523+ajmyers89@users.noreply.github.com>
2023-09-08 16:24:10 -0300

    Add files via upload

--- APP_WEB/views.py
+++ APP_WEB/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Categoria, Producto, Cliente
+from .forms import CategoriaForm, ProductoForm, ClienteForm, BusquedaForm
 
-# Create your views here.
+def agregar_categoria(request):
+    if request.method == 'POST':
+        form = CategoriaForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_categorias')
+    else:
+        form = CategoriaForm()
+    return render(request, 'agregar_categoria.html', {'form': form})
+
+def agregar_producto(request):
+    if request.method == 'POST':
+        form = ProductoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_productos')
+    else:
+        form = ProductoForm()
+    return render(request, 'agregar_producto.html', {'form': form})
+
+def agregar_cliente(request):
+    if request.method == 'POST':
+        form = ClienteForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_clientes')
+    else:
+        form = ClienteForm()
+    return render(request, 'agregar_cliente.html', {'form': form})
+
+def buscar(request):
+    if request.method == 'POST':
+        form = BusquedaForm(request.POST)
+        if form.is_valid():
+            termino_busqueda = form.cleaned_data['termino_busqueda']
+            # Realiza la búsqueda en la base de datos
+            resultados = Producto.objects.filter(nombre__icontains=termino_busqueda)
+            return render(request, 'resultados_busqueda.html', {'resultados': resultados})
+    else:
+        form = BusquedaForm()
+    return render(request, 'buscar.html', {'form': form})
--- Proyecto_web/urls.py
+++ Proyecto_web/urls.py
@@ -20,3 +20,14 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('agregar_categoria/', views.agregar_categoria, name='agregar_categoria'),
+    path('agregar_producto/', views.agregar_producto, name='agregar_producto'),
+    path('agregar_cliente/', views.agregar_cliente, name='agregar_cliente'),
+    path('buscar/', views.buscar, name='buscar'),
+]
